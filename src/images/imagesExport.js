1136125bc67a07f4d00cbe87ef06d672740dcec4
plunkydev <plunkydesign@gmail.com>
2025-01-02 19:18:58 -0500

    Creacion: Se creo la variable que contiene todas la imagenes que contendra el carrusel

---
+++ src/images/imagesExport.js
@@ -0,0 +1,8 @@
+import img1 from './img-1.webp'
+import img2 from './img-2.webp'
+import img3 from './img-3.webp'
+import img4 from './img-4.webp'
+import img5 from './img-5.webp'
+import img6 from './img-6.webp'
+
+export const carruselImages = [img1, img2, img3, img4, img5, img6]
