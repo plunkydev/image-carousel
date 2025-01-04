3efb862c70cd630de68a5e6cb66b0e494f95e28c
plunkydev <plunkydesign@gmail.com>
2025-01-03 21:34:16 -0500

    Se integra la navegación al carrusel: se importa y agrega el componente carruselNav al DOM junto al carruselContainer, permitiendo la interacción del usuario con botones y navegación visual.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './style.css'
 import { carruselContainer } from './components/carrusel.js'
+import { carruselNav } from './components/carruselNav.js'
 const main = document.getElementById('main')
 
-main.append(carruselContainer)
+main.append(carruselContainer, carruselNav)
