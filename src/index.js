3a8cb9c349a4aa53e2ed5bef4f52b7a266ae184f
plunkydev <plunkydesign@gmail.com>
2025-01-05 20:34:00 -0500

    Importacion de actionRight para llamarla cada 5 segundos con un setInterval

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './style.css'
 import { carruselContainer } from './components/carrusel.js'
-import { carruselNav } from './components/carruselNav.js'
+import { carruselNav, actionRight } from './components/carruselNav.js'
 const main = document.getElementById('main')
+
+setInterval(actionRight, 5000)
 
 main.append(carruselContainer, carruselNav)
